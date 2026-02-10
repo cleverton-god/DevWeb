7040667f595e519fc3d3af6d697851ea87cd334e
cleverton <cleverton_rosa150@estudante.sesisenai.org.br>
2026-02-10 19:06:19 -0300

    Tarefa 12 — Criar Botão de Alerta do Sistema

--- desafios/4-Desafio/registro-paranormal/js/script.js
+++ desafios/4-Desafio/registro-paranormal/js/script.js
@@ -1,1 +1,8 @@
 console.log("o sistema foi iniciado.");
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btnParanormal");
+
+  btn.addEventListener("click", () => {
+    alert("⚠️ ALERTA PARANORMAL ⚠️\nO sistema detectou uma presença estranha...");
+  });
+});
