f5f521f5aaaabbb3f731b982568a41cc7860963d
cleverton <cleverton_rosa150@estudante.sesisenai.org.br>
2026-02-10 19:20:33 -0300

    Tarefa 13 — Alterar Texto Dinamicamente

--- desafios/4-Desafio/registro-paranormal/js/script.js
+++ desafios/4-Desafio/registro-paranormal/js/script.js
@@ -1,8 +1,21 @@
-console.log("o sistema foi iniciado.");
+// Aguarda o HTML carregar antes de acessar os elementos
 document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("btnParanormal");
 
-  btn.addEventListener("click", () => {
+  /* ===== Seleção de elementos ===== */
+  const btnParanormal = document.getElementById("btnParanormal");
+  const btnEmergencia = document.getElementById("btnEmergencia");
+  const statusSistema = document.getElementById("statusSistema");
+
+  /* ===== Eventos ===== */
+
+  // Botão: Testar Sistema Paranormal
+  btnParanormal.addEventListener("click", () => {
     alert("⚠️ ALERTA PARANORMAL ⚠️\nO sistema detectou uma presença estranha...");
   });
+
+  // Botão: Ativar Protocolo de Emergência
+  btnEmergencia.addEventListener("click", () => {
+    statusSistema.innerText = "🚨 PROTOCOLO DE EMERGÊNCIA ATIVADO 🚨";
+  });
+
 });
