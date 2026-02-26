2ab9017c2a3683b13a248eb12473346181e83d53
cleverton <cleverton_rosa150@estudante.sesisenai.org.br>
2026-02-26 15:31:56 -0300

    # Exercícios

---
+++ Back-end/Pratica com Express/exercicio4/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+app.use(express.json());
+
+const produtos = [
+    { id: 1, nome: "Mouse", preco: 50 },
+    { id: 2, nome: "Teclado", preco: 120 },
+    { id: 3, nome: "Monitor", preco: 800 }
+  ];
+let contadorId = 1;
+
+app.post("/produtos", (req, res) => {
+  const produto = req.body;
+  produto.id = contadorId++;
+  produtos.push(produto);
+  res.status(201).json(produto);
+});
+
+app.get("/produtos", (req, res) => {
+  res.json(produtos);
+});
+
+app.get("/produtos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const produto = produtos.find(p => p.id === id);
+
+  if (!produto) return res.status(404).send("Produto não encontrado");
+
+  res.json(produto);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
